01e7c5727e7a6a2351b147633c650a141208b3e2
Nikita Prokopov <prokopov@gmail.com>
2018-12-20 03:37:15 +0300

    Moved F* to first two rows

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -33,15 +33,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_ortho_4x12( \
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TILD, \
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_GRV,  KC_LPRN, KC_RPRN, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_PLUS, KC_EQL,  KC_SCLN, KC_COLN, XXXXXXX, XXXXXXX, \
-  _______, _______, _______, _______, _______, _______,    _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______  \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_PLUS, KC_EQL,  KC_SCLN, KC_COLN, XXXXXXX, XXXXXXX, \
+  _______, _______, _______, _______, XXXXXXX, XXXXXXX,    _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______  \
 ),
 
 [_RAISE] = LAYOUT_ortho_4x12( \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX,    LN_BACK, KC_PGUP, KC_UP,   KC_PGDN, LN_FWD,  XXXXXXX, \
-  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,      WRD_BCK, KC_LEFT, KC_DOWN, KC_RGHT, WRD_FWD, XXXXXXX, \
-  _______, XXXXXXX, KC_F10,  KC_F11,  KC_F12,  XXXXXXX,    XXXXXXX, KC_BSPC, XXXXXXX, KC_DEL,  XXXXXXX, XXXXXXX, \
-  _______, _______, _______, _______, _______, _______,    _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______  \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      LN_BACK, KC_PGUP, KC_UP,   KC_PGDN, LN_FWD,  XXXXXXX, \
+  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     WRD_BCK, KC_LEFT, KC_DOWN, KC_RGHT, WRD_FWD, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_BSPC, XXXXXXX, KC_DEL,  XXXXXXX, XXXXXXX, \
+  _______, _______, _______, _______, XXXXXXX, XXXXXXX,    _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______  \
 ),
 
 };
