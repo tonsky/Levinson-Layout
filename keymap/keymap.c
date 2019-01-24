91486162151ce8bc38dc8d6a34d35ae5d6ec7140
Nikita Prokopov <prokopov@gmail.com>
2019-01-24 19:34:51 +0300

    moved media to adjust layer, swapped tab and shift, rearranged grave, tilde, braces, quotes, switched back to ANSI

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -5,6 +5,7 @@ extern keymap_config_t keymap_config;
 #define _QWERTY 0
 #define _LOWER 3
 #define _RAISE 4
+#define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -25,28 +26,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_ortho_4x12( \
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SLSH, \
-  KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_LBRC, KC_RBRC, \
-  KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_NUBS, KC_NUHS, \
-  KC_F17,  KC_LCTL, KC_LALT, KC_LGUI, KC_ENT,  KC_SPC,     RAISE,   LOWER,   KC_MINS, KC_QUOT, KC_DQUO, KC_F18   \
+  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_LBRC, KC_RBRC, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, KC_GRV, \
+  KC_F17,  KC_LCTL, KC_LALT, KC_LGUI, KC_ENT,  KC_SPC,     RAISE,   LOWER,   KC_SCLN, KC_COLN, KC_MINS, KC_F18   \
 ),
 
 [_LOWER] = LAYOUT_ortho_4x12( \
-  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TILD, \
-  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_GRV,  KC_LPRN, KC_RPRN, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_PLUS, KC_EQL,  KC_SCLN, KC_COLN, XXXXXXX, XXXXXXX, \
-  _______, _______, _______, _______, XXXXXXX, XXXXXXX,    _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______  \
+  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PIPE, \
+  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_BSLS, KC_LPRN, KC_RPRN, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    _______, KC_PLUS, KC_EQL,  _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______  \
 ),
 
 [_RAISE] = LAYOUT_ortho_4x12( \
-  _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,       KC_F5,     LN_BACK, KC_PGUP, KC_UP,   KC_PGDN, LN_FWD,  XXXXXXX, \
-  _______, KC_F6,   KC_F7,   KC_F8,    KC_F9,       KC_F10,    WRD_BCK, KC_LEFT, KC_DOWN, KC_RGHT, WRD_FWD, XXXXXXX, \
-  _______, KC_F11,  KC_F12,  KC__MUTE, KC__VOLDOWN, KC__VOLUP, XXXXXXX, KC_BSPC, XXXXXXX, KC_DEL,  XXXXXXX, XXXXXXX, \
-  _______, _______, _______, _______,  XXXXXXX,     XXXXXXX,   _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______  \
+  _______, _______, _______, _______, _______, _______,    LN_BACK, KC_PGUP, KC_UP,   KC_PGDN, LN_FWD,  XXXXXXX, \
+  _______, _______, _______, _______, _______, _______,    WRD_BCK, KC_LEFT, KC_DOWN, KC_RGHT, WRD_FWD, XXXXXXX, \
+  _______, _______, _______, _______, _______, _______,    XXXXXXX, KC_BSPC, XXXXXXX, KC_DEL,  XXXXXXX, XXXXXXX, \
+  _______, _______, _______, _______, _______, _______,    _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______  \
 ),
 
+[_ADJUST] = LAYOUT_ortho_4x12( \
+  _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,       KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX, \
+  _______, XXXXXXX, XXXXXXX, KC_MRWD,  KC_MPLY,     KC_MFFD,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  _______, XXXXXXX, XXXXXXX, KC__MUTE, KC__VOLDOWN, KC__VOLUP,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  _______, _______, _______, _______,  _______,     _______,      _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______  \
+),
 };
-
-// KC_MRWD, KC_MPLY, KC_MFFD, 
 
 void persistent_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
@@ -64,16 +69,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
