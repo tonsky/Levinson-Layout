259385f4769d848698b0adf56118acc31b8200ec
Nikita Prokopov <prokopov@gmail.com>
2019-04-05 17:59:29 +0300

    TISIconIsTemplate and TISCapsLockIsSwitch

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -ev
 
-iconutil --convert icns --output Levinson.bundle/Contents/Resources/English\ -\ Levinson.icns English\ Light.iconset
-iconutil --convert icns --output Levinson.bundle/Contents/Resources/Russian\ -\ Levinson.icns Russian\ Light.iconset
-# iconutil --convert icns --output Levinson.bundle/Contents/Resources/English\ -\ Levinson\ Dark.icns English\ Dark.iconset
-# iconutil --convert icns --output Levinson.bundle/Contents/Resources/Russian\ -\ Levinson\ Dark.icns Russian\ Dark.iconset
+iconutil --convert icns --output Levinson.bundle/Contents/Resources/English\ -\ Levinson.icns English.iconset
+iconutil --convert icns --output Levinson.bundle/Contents/Resources/Russian\ -\ Levinson.icns Russian.iconset
 
 touch Levinson.bundle
 
-rm -rf ~/Library/Keyboard\ Layouts/Levinson.bundle
-cp -r Levinson.bundle /Library/Keyboard\ Layouts/
+sudo rm -rf /Library/Keyboard\ Layouts/Levinson.bundle
+sudo cp -r Levinson.bundle /Library/Keyboard\ Layouts/
